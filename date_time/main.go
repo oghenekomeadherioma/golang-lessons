84ef0e0f52de9d23b289e5971e65fc80f9fd07b0
Mustapha Alayande <alayandemustapha@gmail.com>
2025-04-23 00:42:58 +0200

    Working with Date time

--- date_time/main.go
+++ date_time/main.go
@@ -7,4 +7,17 @@ import (
 func main() {
 	dob := time.Date(1940, time.October, 18, 9, 27, 6, 7, time.UTC)
 	fmt.Println(dob)
+	fmt.Println(dob.Format("2025-05-05"))
+	fmt.Println(dob.Format("2006-01-02"))
+	fmt.Println(dob.Format("Mon, Jan, 02"))
+	 d := 35000 * time.Millisecond
+	fmt.Println(d)
+	tz, _ := time.LoadLocation("America/New_York")
+	fmt.Println(tz)
+
+	// Conver t one time zone to another
+	lag_time := time.Date(2025, time.April, 21, 12, 0, 0, 0, time.UTC)
+	fmt.Println(lag_time)
+
+	// 
 }
