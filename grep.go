35b6f7fd1b3d7d2526f3e01b385c4d369778e92c
Mustapha Alayande <alayandemustapha@gmail.com>
2025-05-06 04:15:49 +0200

    Implementing grep with golang

--- grep.go
+++ grep.go
@@ -10,3 +10,24 @@ import (
 	"strings"
 )
 
+func main() {
+
+}
+
+func grep(file io.Reader, term string) (string, error) {
+		match := []string
+		scan, err := bufio.NewScanner(file)
+		if err != nil {
+			fmt.Println(err)
+		}
+		for scan.Scan {
+			if strings.Contains(scan.Text, term) {
+				match = append(match, scan.Text())
+			}
+		}
+		if err := s.Err(); err != nil {
+			return []string{}, err
+		}
+		return match, nil
+}
+
