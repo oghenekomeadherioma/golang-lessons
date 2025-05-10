721e7a96d1456ddd6bf748c537cf8b0f3428b0b1
Mustapha Alayande <alayandemustapha@gmail.com>
2025-05-10 10:54:57 +0200

    Using structs

--- talld/edit_ops.go
+++ talld/edit_ops.go
@@ -8,18 +8,25 @@ func main() {
 
 }
 
-type Instance struct {
+type instance struct {
 	original_list []string
 	deleted_list  []string
 	next_add string
 	last_add string
 }
 
-func add(alist *[]string, item string) *[]string{
-	add_pos := append(*alist, item)
-	fmt.Println("Added item at position", len(*alist), ":", add_pos)
-	return &add_pos
+func add(a_instance *instance) instance{
+	a_instance.original_list = append(a_instance.original_list, a_instance.next_add)
+	//fmt.Println("Added item at position", len(*alist), ":", add_pos)
+	return *a_instance
 }
+
+
+// func add(alist *[]string, item string) *[]string{
+// 	add_pos := append(*alist, item)
+// 	fmt.Println("Added item at position", len(*alist), ":", add_pos)
+// 	return &add_pos
+// }
 
 // delete removes the last element from the slice `alist` and appends it to a 
 // separate slice `deleted`. It then prints the deleted item and its position.
